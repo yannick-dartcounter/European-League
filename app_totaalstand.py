f965ee9856142d9fa72744aee92d76042d33302e
yannick-dartcounter <yannick.dartcounter@outlook.com>
2025-07-10 20:42:42 +0200

    Update app_totaalstand.py

--- app_totaalstand.py
+++ app_totaalstand.py
@@ -43,7 +43,7 @@ df.rename(columns={
 }, inplace=True)
 
 # 🚫 Verberg index (0,1,2...) in st.table
-st.dataframe(df.style.hide(axis="index"), use_container_width=True)
+df.index = [""] * len(df)
 
 # 🕒 Laatste update tonen
 st.caption(f"📅 Last updated: {last_updated.strftime('%d-%m-%Y %H:%M:%S')} UTC")
